da56ec65bf23ff0eaa8437870c9d5fa4a03719fa
fossildog117 <nathanliu@XaEs.local>
2017-01-14 13:53:47 +0000

    sql/password vulnerabilities

--- Web/registration.php
+++ Web/registration.php
@@ -11,16 +11,13 @@ require('db.php');
 // If form submitted, insert values into the database.
 if (isset($_REQUEST['username'])){
         // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
+	$username = $_REQUEST['username'];
         //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username);
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($con,$email);
-	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
+	$email = $_REQUEST['email'];
+	$password = $_REQUEST['password'];
 	$trn_date = date("Y-m-d H:i:s");
         $query = "INSERT into `users` (username, password, email, trn_date)
-VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+VALUES ('$username', '".$password."', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
         if($result){
             echo "<div class='form'>
