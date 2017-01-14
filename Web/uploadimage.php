14f50a1c188d7107803754c8c9f4dbdfd1d6bc35
fossildog117 <quantumbiology@hotmail.co.uk>
2017-01-14 13:58:53 +0000

    removed image size limit and file type

--- Web/uploadimage.php
+++ Web/uploadimage.php
@@ -23,7 +23,7 @@ $file_size = $_FILES['fileToUpload']['size'];
  $final_file=str_replace(' ','-',$new_file_name);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = $_FILES["fileToUpload"]["tmp_name"];
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -42,12 +42,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
